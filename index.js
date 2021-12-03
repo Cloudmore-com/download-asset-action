3e3caf9fcd9b2ecc3468ad2aefe78d05ac406869
petr.lebedev <petr.lebedev@cloudmore.com>
2021-12-03 12:34:55 +0200

    fixesssss

--- index.js
+++ index.js
@@ -1,64 +1,76 @@
-const core = require('@actions/core');
+const core = require("@actions/core");
 const { Octokit } = require("@octokit/rest");
-const { createActionAuth } = require("@octokit/auth-action");
 const { save } = require("save-file");
-const path = require('path');
+const path = require("path");
+
+function requestLogger(httpModule) {
+  var original = httpModule.request;
+  httpModule.request = function (options, callback) {
+    if (options.host.includes("objects.githubusercontent")) {
+      delete options.headers["authorization"];
+    }
+
+    return original(options, callback);
+  };
+}
+
+requestLogger(require("http"));
+requestLogger(require("https"));
 
 async function run() {
-    try {
-        // Authentication
-        const octokit = new Octokit({
-            authStrategy: createActionAuth
-        });
+  try {
+    // Authentication
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
+    console.log("1");
+    // Inputs
+    const [owner, repo] = ["Cloudmore-com", "cmp"]; //core.getInput('repository').split("/");
+    const excludes = []; //core.getInput('excludes').trim().split(",");
+    const tag = "release-3.51.0"; //core.getInput('tag');
+    const assetName = "liquibase_patches"; //core.getInput('asset');
+    const target = "."; //core.getInput('target');
 
-        // Inputs
-        const [owner, repo] = core.getInput('repository').split("/");
-        const excludes = core.getInput('excludes').trim().split(",");
-        const tag = core.getInput('tag');
-        const assetName = core.getInput('asset');
-        const target = core.getInput('target');
-
-        // Get release
-        let releases  = await octokit.repos.listReleases({
-            owner: owner,
-            repo: repo,
-        });
-        releases = releases.data;
-
-        if (excludes) {
-            if (excludes.includes('prerelease')) 
-                releases = releases.filter(rel => rel.prerelease != true);
-            if (excludes.includes('draft')) 
-                releases = releases.filter(rel => rel.draft != true);
-        }
-        if (tag)
-            releases = releases.filter(rel => rel.tag_name.includes(tag));
-        if (releases.length === 0)
-            throw new Error("No matching releases");
-
-        // Get asset
-        let assets = releases[0].assets;
-        assets = assets.filter(ass => ass.name.includes(assetName));
-
-        if (assets.length === 0)
-            throw new Error("No matching assets");
-
-        asset = await octokit.repos.getReleaseAsset({
-            headers: {
-                Accept: "application/octet-stream"
-            },
-            owner: owner,
-            repo: repo,
-            asset_id: assets[0].id
-        });
-
-        // Save asset to target and set output
-        await save(asset.data, path.join(target, assets[0].name));
-        core.setOutput('name', assets[0].name)
+    // Get release
+    console.log("2");
+    let releases = await octokit.repos.listReleases({
+      owner: owner,
+      repo: repo,
+    });
+    releases = releases.data;
+    if (excludes) {
+      if (excludes.includes("prerelease"))
+        releases = releases.filter((rel) => rel.prerelease != true);
+      if (excludes.includes("draft"))
+        releases = releases.filter((rel) => rel.draft != true);
     }
-    catch (error) {
-        core.setFailed(error.message);
-    }
+    if (tag) releases = releases.filter((rel) => rel.tag_name.includes(tag));
+    if (releases.length === 0) throw new Error("No matching releases");
+
+    console.log("3");
+    // Get asset
+    let assets = releases[0].assets;
+    assets = assets.filter((ass) => ass.name.includes(assetName));
+
+    if (assets.length === 0) throw new Error("No matching assets");
+
+    console.log("4");
+    asset = await octokit.repos.getReleaseAsset({
+      headers: {
+        Accept: "application/octet-stream",
+        authorization: null,
+      },
+      owner: owner,
+      repo: repo,
+      asset_id: assets[0].id,
+    });
+    await save(asset.data, path.join(target, assets[0].name));
+
+    core.setOutput("name", assets[0].name);
+  } catch (error) {
+    console.log("error");
+    core.setFailed(error.message);
+  }
 }
 
 run();
